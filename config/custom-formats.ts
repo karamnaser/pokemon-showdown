884609eada24f4ac323ada29c8739d6c4c6f0a0a
karamnaser <karam_m_n@hotmail.com>
2023-07-11 15:33:43 +0300

    adding mode to config/format.ts,adding data/mode/digimon

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -5,7 +5,7 @@ export const Formats: FormatList = [
 		column: 2,
 	},
 	{
-		name: "[Gen6d] OU",
+		name: "[Gen 9] Ubers",
 		desc: "Battle and play with your friends with this Digimon & LGPE hyrbid.",
 		mod: "digimon",
 		ruleset: ['Standard'],
