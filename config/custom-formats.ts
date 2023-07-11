9d93bd293c6192814713ffea10c40a147a779ed4
karamnaser <karam_m_n@hotmail.com>
2023-07-11 20:59:13 +0300

    add seasonla to name of mode

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -5,7 +5,7 @@ export const Formats: FormatList = [
 		column: 2,
 	},
 	{
-		name: ["Digimon"],
+		name: "[Digimon]",
 		desc: "Battle and play with your friends with this Digimon & LGPE hyrbid.",
 		mod: "digimon",
 		ruleset: ['Standard','+ OU','- all Pokemon'],
