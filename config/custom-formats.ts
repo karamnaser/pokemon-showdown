d847ca81a891e96a13294cb9c3e5411a033699a5
karamnaser <karam_m_n@hotmail.com>
2023-07-11 09:23:56 +0300

    adding mode to config/format.ts,adding data/mode/digimon

---
+++ config/custom-formats.ts
@@ -0,0 +1,144 @@
+export const Formats: FormatList = [
+	// Digimon Metas
+	{
+		section: "Digimon Metas",
+		column: 2,
+	},
+	{
+		name: "[Digimon] Digimon Showdown Singles",
+		desc: "Battle and play with your friends with this Digimon & LGPE hyrbid.",
+
+		mod: "digimon",
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+	{
+		name: "[Digimon] Digimon Showdown Doubles",
+		desc: "Battle and play with your friends with this Digimon & LGPE hyrbid.",
+
+		mod: "digimon",
+		gameType: 'doubles',
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+	{
+		name: "[Digimon] Digimon Showdown Triples",
+		desc: "Battle and play with your friends with this Digimon & LGPE hyrbid.",
+
+		mod: "digimon",
+		gameType: 'triples',
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+	{
+		name: "[Digimon] Digimon Showdown Monotype",
+		desc: "Battle in this Digimon & LGPE Monotype format!",
+
+		mod: "digimon",
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Same Type Clause', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+	{
+		name: "[Digimon] Digimon Showdown Monotype Doubles",
+		desc: "Battle in this Digimon & LGPE Monotype format!",
+
+		mod: "digimon",
+		gameType: 'doubles',
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Same Type Clause', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+	{
+		name: "[Digimon] Digimon x Pokemon",
+		desc: "Worlds Collide in this Digimon & LGPE Format!",
+
+		mod: "digimon",
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+	{
+		name: "[Digimon] Digimon x Pokemon Doubles",
+		desc: "Worlds Collide in this Digimon & LGPE Format!",
+
+		mod: "digimon",
+		gameType: 'doubles',
+		team: "randomDigimon",
+		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Terastal Clause'],
+		onBegin() {
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onAfterMega(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+		onUpdate(pokemon) {
+			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
+		},
+	},
+];
