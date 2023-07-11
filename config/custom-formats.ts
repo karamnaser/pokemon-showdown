10e39703116f21d36f88320cb3f47d503ef5f884
karamnaser <karam_m_n@hotmail.com>
2023-07-11 10:59:46 +0300

    adding mode to config/format.ts,adding data/mode/digimon

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -6,21 +6,9 @@ export const Formats: FormatList = [
 	{
 		name: "[Digimon] Digimon Showdown Singles",
 		desc: "Battle and play with your friends with this Digimon & LGPE hyrbid.",
-
 		mod: "digimon",
 		team: "randomDigimon",
 		ruleset: ['Cancel Mod', 'Dynamax Clause', 'HP Percentage Mod', 'Team Preview', 'Terastal Clause'],
-		onBegin:()=> {
-		},
-		onSwitchIn:(pokemon)=> {
-			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
-		},
-		onAfterMega:(pokemon)=> {
-			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
-		},
-		onUpdate:(pokemon)=> {
-			this.add('-start', pokemon, 'typechange', pokemon.types.join('/'), '[silent]');
-		},
 	},
 ];
 
